00f6cde51fac3be17330b6b9c8b40276280acfce
Minal Pudasainee <pudasaineeminal@gmail.com>
2025-04-21 00:59:47 +0545

    side bar problem

--- src/components/homePage/header.tsx
+++ src/components/homePage/header.tsx
@@ -1,26 +1,137 @@
-import React from 'react'
+import { Github, Instagram, Linkedin, Star, Twitter } from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='flex flex-row w-full mt-4'>
+    <div className="flex flex-col md:flex-row w-full pt-2 bg-zinc-100 dark:bg-zinc-900 px-2 md:gap-2">
       {/* movie slider */}
-      <div>
-        movie slider
+      <div className="w-full md:w-full h-[500px] relative max-w-4xl">
+        <Image
+          src="/dune-2b.jpg"
+          alt="Dune 2"
+          width={400}
+          height={100}
+          className="w-full h-[500px] inset-0 absolute aspect-square max-w-4xl object-cover mx-auto shadow-lg "
+        />
+        <div className="flex flex-col justify-start w-full top-82 p-6 text-white relative z-20 bg-gradient-to-b from-zinc-700/95 via-zinc-800/95 to-zinc-950 ">
+          <h2 className="text-4xl font-bold text-left">{"Movie Title"}</h2>
+          <div className="flex items-center space-x-2 mt-4 gap-6">
+            <span className="text-yellow-400 flex items-center">
+              <Star className="mr-1 text-yellow-400" />
+              {10}
+            </span>
+            <span className="bg-gray-700 px-2 py-1 rounded">{`NP`}</span>
+          </div>
+          <p className="mt-4 text-sm text-white line-clamp-2 text-left">
+            {`first rule of fight club is that you don't talk about fight club`}
+          </p>
+        </div>
       </div>
 
       {/* random 3 movies */}
-      <div>
-        random 3 movies
+      <div className="w-full md:w-[40%] flex flex-col space-y-6 p-3 cursor-pointer">
+        <div className="flex p-3 rounded-lg shadow-md hover:scale-105 transition duration-100 dark:bg-zinc-800">
+          <Image
+            className="w-20 h-24 object-cover rounded"
+            src="/dune-2.jpg"
+            // TMDB image URL
+            alt={"Movie title"}
+            width={80}
+            height={120}
+          />
+          <div className="ml-4 text-black dark:text-white">
+            <div className="flex items-center space-x-2">
+              <span className="text-yellow-400 flex items-center">
+                <Star className="mr-1" /> {`10`}
+              </span>
+              <span className="bg-gray-700 px-2 py-1 rounded text-white">
+                {"NP"}
+              </span>
+            </div>
+            <h3 className="text-[18px] font-semibold mt-1 line-clamp-1">
+              {"Movie Title"}
+            </h3>
+            <p className="text-[13px] text-gray-900 dark:text-white line-clamp-2">
+              {`first ule of fight club is that you dont talk about fight club`}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex p-3 rounded-lg shadow-md hover:scale-105 transition duration-100 dark:bg-zinc-800">
+          <Image
+            className="w-20 h-24 object-cover rounded"
+            src="/dune-2.jpg"
+            // TMDB image URL
+            alt={"Movie title"}
+            width={80}
+            height={120}
+          />
+          <div className="ml-4 text-black dark:text-white">
+            <div className="flex items-center space-x-2">
+              <span className="text-yellow-400 flex items-center">
+                <Star className="mr-1" /> {`10`}
+              </span>
+              <span className="bg-gray-700 px-2 py-1 rounded text-white">
+                {"NP"}
+              </span>
+            </div>
+            <h3 className="text-[18px] font-semibold mt-1 line-clamp-1">
+              {"Movie Title"}
+            </h3>
+            <p className="text-[13px] text-gray-900 dark:text-white line-clamp-2">
+              {`first ule of fight club is that you dont talk about fight club`}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex p-3 rounded-lg shadow-md hover:scale-105 transition duration-100 dark:bg-zinc-800">
+          <Image
+            className="w-20 h-24 object-cover rounded"
+            src="/dune-2.jpg"
+            // TMDB image URL
+            alt={"Movie title"}
+            width={80}
+            height={120}
+          />
+          <div className="ml-4 text-black dark:text-white">
+            <div className="flex items-center space-x-2">
+              <span className="text-yellow-400 flex items-center">
+                <Star className="mr-1" /> {`10`}
+              </span>
+              <span className="bg-gray-700 px-2 py-1 rounded text-white">
+                {"NP"}
+              </span>
+            </div>
+            <h3 className="text-[18px] font-semibold mt-1 line-clamp-1">
+              {"Movie Title"}
+            </h3>
+            <p className="text-[13px] text-gray-900 dark:text-white line-clamp-2">
+              {`first ule of fight club is that you dont talk about fight club`}
+            </p>
+          </div>
+        </div>
       </div>
 
       {/* social */}
 
-      <div>
-        social
+      <div className="w-full md:w-[10%] flex flex-row justify-center md:flex-col md:items-center gap-6 p-4 text-black dark:text-white">
+        {[
+          { icon: Twitter, name: "Twitter" },
+          { icon: Instagram, name: "Instagram" },
+          { icon: Github, name: "GitHub" },
+          { icon: Linkedin, name: "LinkedIn" },
+        ].map((social, index) => (
+          <button
+            key={index}
+            className="flex items-center flex-row  p-2 rounded"
+          >
+            <social.icon className="text-4xl hover:scale-125 transition duration-100" />
+          </button>
+        ))}
       </div>
+      </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Header
+export default Header;
--- src/components/homePage/navbar.tsx
+++ src/components/homePage/navbar.tsx
@@ -1,15 +1,16 @@
 "use client";
 import { SignedIn, UserButton } from "@clerk/nextjs";
-import { Menu, Moon, Search, Sun } from "lucide-react";
+import { Menu, Moon, Search, Sun, XCircle } from "lucide-react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { LampContainer } from "../ui/lamp";
+import { motion } from "motion/react";
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
 
   const isDark = theme === "dark";
-
 
   const [hideOnScroll, setHideOnScroll] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
@@ -20,7 +21,7 @@ const Navbar = () => {
       const currentScrollY = window.scrollY;
       setScrolled(window.scrollY > 10);
 
-      if (currentScrollY > lastScrollY && currentScrollY > 10) {
+      if (currentScrollY > lastScrollY || currentScrollY > 10) {
         setHideOnScroll(true);
       } else {
         setHideOnScroll(false);
@@ -33,27 +34,84 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [lastScrollY]);
 
+  const sideMenuRef = useRef<HTMLUListElement | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openMenu = () => {
+    setIsOpen(true);
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(-16rem)";
+    }
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(16rem)";
+    }
+  };
+
   return (
-    <nav className={`fixed top-0 z-50 flex mx-auto justify-between bg-white/90 dark:bg-zinc-900 items-center transition-all duration-300 shadow-md ${scrolled? "py-1 rounded-full scale-90 w-[70%] top-4 backdrop-blur-md" : "py-1 scale-100 w-[100%]"}`}>
+    <nav
+      className={`sticky top-0 z-50 flex mx-auto justify-between bg-neutral-200 dark:bg-zinc-900 items-center transition-all duration-300 px-2 ${
+        scrolled
+          ? "py-1 rounded-full opacity-90 scale-90 w-[70%] top-4 backdrop-blur-lg"
+          : "py-1 scale-100 w-[100%]"
+      }`}
+    >
       <div className="flex justify-center items-center gap-10">
         <div className="flex justify-center items-center gap-6 ml-4">
-          <span className={`${hideOnScroll? "opacity-0 pointer-events-none":"opacity-100"}`}>
+          <span
+            onClick={openMenu}
+            className={`${
+              hideOnScroll ? "opacity-0 pointer-events-none" : "opacity-100"
+            }`}
+          >
             <Menu />
           </span>
+
+          {isOpen && (
+            <div className={`${scrolled?"hidden" :"flex flex-col absolute items-center left-0 top-0 bottom-0 w-64 z-50 h-screen transition duration-1000"} `}>
+
+            <LampContainer>
+            
+            <div
+              onClick={closeMenu}
+              className="p-1 cursor-pointer absolute right-6 top-6 dark:hover:rounded-full dark:hover:bg-slate-900/70 dark:hover:text-white"
+            >
+              <span><XCircle size={36} className="w-10" /></span>
+            </div>
+              <motion.h1
+                initial={{ opacity: 0.5, y:10 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  delay: 0.1,
+                  duration: 0.1,
+                }}
+                className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+                >
+                Build lamps <br /> the right way
+              </motion.h1>
+            </LampContainer>
+                </div>
+          )}
+
           <Image
-            src="/logo-white.png"
-            alt="logo-white"
+            src={isDark ? "/logo-white.png" : "/logo-black.png"}
+            alt="chalchitra-logo"
             height={60}
             width={60}
           />
         </div>
-        <form className={`hidden lg:flex items-center ml-10 transition-all duration-300 ${
+        <form
+          className={`hidden lg:flex items-center ml-10 transition-all duration-300 ${
             hideOnScroll ? "opacity-0 pointer-events-none" : "opacity-100"
-          }`}>
+          }`}
+        >
           <input
             type="text"
             placeholder="Search Keywords..."
-            className="w-120 px-4 p-2 rounded-l-full border-solid-black bg-zinc-700 placeholder:text-white  text-white dark:bg-white/90 dark:text-black dark:placeholder:text-zinc-800 focus:placeholder:opacity-0 focus:outline-none"
+            className="w-130 px-4 p-2 rounded-l-full border-solid-black bg-zinc-700 placeholder:text-white  text-white dark:bg-white/90 dark:text-black dark:placeholder:text-zinc-800 focus:placeholder:opacity-0 focus:outline-none"
           />
           <span className="bg-zinc-950 p-2 rounded-r-full text-white">
             <Search />
