3d4a8f8e7943ba0f4370a84a5fea77c85834fd45
Minal Pudasainee <pudasaineeminal@gmail.com>
2025-06-04 20:56:28 +0545

    deployment error

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,16 +3,15 @@ import Footer from '@/components/homePage/footer'
 import Header from '@/components/homePage/header'
 import MainSection from '@/components/homePage/main-section'
 import Navbar from '@/components/homePage/navbar'
-import React, { useState } from 'react'
+import React from 'react'
 
 const Page = () => {
 
-  const [type]= useState<'movies' | 'tvshows'>('movies');
 
   return (
     <main className="flex flex-col w-full h-auto">
       <Navbar/>
-      <Header type={type}/>
+      <Header/>
       <MainSection/>
       <Footer/>
     </main>
