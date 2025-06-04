c6e02a0aa7a1a97121183740ac6c284835002bb2
Minal Pudasainee <pudasaineeminal@gmail.com>
2025-06-04 20:49:05 +0545

    for deployment

--- src/app/search/[query]/page/[page]/page.tsx
+++ src/app/search/[query]/page/[page]/page.tsx
@@ -8,11 +8,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-type Params = {
-  params: { query: string; page: string };
-};
+// type Params = {
+//   params: { query: string; page: string };
+// };
 
-export default async function SearchResultsPage({ params }: Params) {
+export default async function SearchResultsPage({ params }: { params: { query: string; page: string } }) {
   const { query, page } = params;
 
   const res = await fetch(
@@ -24,12 +24,9 @@ export default async function SearchResultsPage({ params }: Params) {
 
   const data = await res.json();
 
-
-
   return (
     <main className="flex flex-col justify-center w-full h-auto">
-
-       {/* --------------------navbar------------------- */}
+      {/* --------------------navbar------------------- */}
       <div className="flex flex-col items-center justify-center w-full">
         <Navbar />
       </div>
@@ -53,60 +50,68 @@ export default async function SearchResultsPage({ params }: Params) {
                 poster_path?: string;
                 backdrop_path?: string;
                 vote_average?: number;
-                release_date?:string;
-                first_air_date?:string;
-                media_type:string;
+                release_date?: string;
+                first_air_date?: string;
+                media_type: string;
               }) => {
-                  const title=item.title||item.name;
-                  const date= item.release_date || item.first_air_date;
-                  const slug = `${title.toLowerCase().replace(/[^a-z0-9]+/g, "-")}-${item.id}`;
-              
-                return(
-               <div key={item.id} className="bg-neutral-200 dark:bg-zinc-900 rounded-xl overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-200 group">
-          {/* Poster */}
-          <div className="relative w-full aspect-[2/3]">
-            <GlowingEffect
-              spread={60}
-              glow
-              disabled={false}
-              proximity={64}
-              inactiveZone={0.01}
-            />
-            <Image
-              src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-              alt={title || "poster"}
-              fill
-              className="object-cover group-hover:brightness-50 transition duration-300"
-              sizes="(max-width: 768px) 100vw, 200px"
-            />
-            <span className="absolute top-2 left-2 flex items-center gap-1 bg-black/60 text-yellow-400 px-2 py-1 rounded-md text-xs sm:text-sm font-kanit z-10">
-              <Star className="w-4 h-4 fill-amber-400" /> {item.vote_average?.toFixed(1)}
-            </span>
-          </div>
-    
-          {/* Info Section */}
-          <div className="flex flex-col justify-between px-3 py-3 h-[110px]">
-            {/* Release date & media type */}
-            <div className="text-sm text-left text-gray-600 dark:text-gray-300 font-kanit mb-1 flex gap-4">
-              {item.media_type && <span className="capitalize">{item.media_type}</span>}
-              {date && <span>  {date?.slice(0,4)}</span>}
-            </div>
-    
-            {/* Title */}
-            <p className="font-outfit text-sm sm:text-base font-semibold text-center line-clamp-1 text-black dark:text-white">
-              {title}
-            </p>
-    
-            {/* Button */}
-            <Link href={`/movies/${slug}`}>
-              <button className="mt-2 w-full py-1.5 text-xs sm:text-sm font-kanit bg-white dark:bg-zinc-800 text-black dark:text-white rounded hover:bg-yellow-500 hover:text-black transition duration-300 hover:scale-95">
-                View Details
-              </button>
-            </Link>
-          </div>
-        </div>
-        );
-            }
+                const title = item.title || item.name;
+                const date = item.release_date || item.first_air_date;
+                const slug = `${title
+                  .toLowerCase()
+                  .replace(/[^a-z0-9]+/g, "-")}-${item.id}`;
+
+                return (
+                  <div
+                    key={item.id}
+                    className="bg-neutral-200 dark:bg-zinc-900 rounded-xl overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-200 group"
+                  >
+                    {/* Poster */}
+                    <div className="relative w-full aspect-[2/3]">
+                      <GlowingEffect
+                        spread={60}
+                        glow
+                        disabled={false}
+                        proximity={64}
+                        inactiveZone={0.01}
+                      />
+                      <Image
+                        src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                        alt={title || "poster"}
+                        fill
+                        className="object-cover group-hover:brightness-50 transition duration-300"
+                        sizes="(max-width: 768px) 100vw, 200px"
+                      />
+                      <span className="absolute top-2 left-2 flex items-center gap-1 bg-black/60 text-yellow-400 px-2 py-1 rounded-md text-xs sm:text-sm font-kanit z-10">
+                        <Star className="w-4 h-4 fill-amber-400" />{" "}
+                        {item.vote_average?.toFixed(1)}
+                      </span>
+                    </div>
+
+                    {/* Info Section */}
+                    <div className="flex flex-col justify-between px-3 py-3 h-[110px]">
+                      {/* Release date & media type */}
+                      <div className="text-sm text-left text-gray-600 dark:text-gray-300 font-kanit mb-1 flex gap-4">
+                        {item.media_type && (
+                          <span className="capitalize">{item.media_type}</span>
+                        )}
+                        {date && <span> {date?.slice(0, 4)}</span>}
+                      </div>
+
+                      {/* Title */}
+                      <p className="font-outfit text-sm sm:text-base font-semibold text-center line-clamp-1 text-black dark:text-white">
+                        {title}
+                      </p>
+
+                      {/* Button */}
+                      <Link href={`/movies/${slug}`}>
+                        <button className="mt-2 w-full py-1.5 text-xs sm:text-sm font-kanit bg-white dark:bg-zinc-800 text-black dark:text-white rounded hover:bg-yellow-500 hover:text-black transition duration-300 hover:scale-95">
+                          View Details
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                );
+              }
             )}
           </div>
         </TracingBeam>
